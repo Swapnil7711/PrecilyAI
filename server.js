760c91eba9d29c7a2e2ed8415c891043fbddeeaa
swapnil7711 <swapnil7711@gmail.com>
2022-01-12 17:30:44 +0530

    deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { DB_URL } from "./config/index.js"
 import router from "./routes/index.js"
+import path from "path"
 import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import { errorHandler } from "./middlewares/errorHandler.js"
